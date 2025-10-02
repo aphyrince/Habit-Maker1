c1d767330d82b69c936ab0fb6894d469558c86ff
aphyrince <dkswjdgh01@naver.com>
2025-10-02 14:41:15 +0900

    리스트 페이지 디자인

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./Footer/Footer";
+import Header from "./header/Header";
+import ItemList from "./ItemList/ItemList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="page">
+            <Header />
+            <ItemList />
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
