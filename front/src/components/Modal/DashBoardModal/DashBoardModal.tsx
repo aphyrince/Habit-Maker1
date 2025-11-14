c7f199c5422a9b9f29c1c48efd2843f601b96d6f
aphyrince <dkswjdgh01@naver.com>
2025-11-14 18:15:12 +0900

    대쉬보드 기능, 디자인 완성

--- front/src/components/Modal/DashBoardModal/DashBoardModal.tsx
+++ front/src/components/Modal/DashBoardModal/DashBoardModal.tsx
@@ -1,5 +1,5 @@
 import "./DashBoardModal.css";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { ItemData } from "../../../utils/types";
 import {
     Bar,
@@ -9,6 +9,8 @@ import {
     XAxis,
     YAxis,
 } from "recharts";
+import { Save } from "lucide-react";
+import { useItems } from "../../../context/Items/ItemsContext";
 
 const DashBoardModal = ({ item }: { item: ItemData }) => {
     const [commentText, setCommentText] = useState<string>(item.comment);
@@ -17,6 +19,7 @@ const DashBoardModal = ({ item }: { item: ItemData }) => {
         const today = new Date();
         return new Date(today.getFullYear(), today.getMonth(), 1);
     });
+    const { update } = useItems();
 
     const chartData = useMemo(() => {
         const year = currentMonth.getFullYear();
@@ -43,30 +46,56 @@ const DashBoardModal = ({ item }: { item: ItemData }) => {
         return data;
     }, [item.logList, currentMonth]);
 
+    const handleSave = useCallback(() => {
+        setEditMode(false);
+        item = { ...item, comment: commentText };
+        update(item);
+    }, [item.comment, commentText]);
+
+    const handleMonthChange = (offset: number) => {
+        setCurrentMonth((prev) => {
+            const newMonth = new Date(prev);
+            newMonth.setMonth(prev.getMonth() + offset);
+            return newMonth;
+        });
+    };
+
     return (
         <div
             className="modal-overlay"
             style={{ borderTop: `10px solid ${item.bgColor}` }}
         >
             <h2 className="modal-title">{item.text}</h2>
-            <div className="modal-status">
+            <div
+                className={`modal-status ${
+                    item.isChecked ? "status-done" : "status-progress"
+                }`}
+            >
                 {item.isChecked ? "완료됨" : "미완료"}
             </div>
             <div className="modal-section">
                 <div className="month-nav">
-                    <button className="month-btn" title="이전 달">
+                    <button
+                        className="month-btn"
+                        title="이전 달"
+                        onClick={() => handleMonthChange(-1)}
+                    >
                         {"<"}
                     </button>
                     <h3>{currentMonth.getMonth() + 1}</h3>
-                    <button className="month-btn" title="다음 달">
+                    <button
+                        className="month-btn"
+                        title="다음 달"
+                        onClick={() => handleMonthChange(1)}
+                    >
                         {">"}
                     </button>
                 </div>
                 <div className="chart-wrapper">
-                    <ResponsiveContainer width="100%" height={180}>
+                    <ResponsiveContainer width="100%" height={120}>
                         <BarChart data={chartData}>
                             <XAxis dataKey="date" tick={{ fontSize: 9 }} />
-                            <YAxis width={25} ticks={[0, 1, 2]} />
+                            <YAxis width={25} ticks={[0, 1]} />
                             <Tooltip />
                             <Bar
                                 dataKey="count"
@@ -80,19 +109,28 @@ const DashBoardModal = ({ item }: { item: ItemData }) => {
             <div className="modal-section">
                 <h3>코멘트</h3>
                 {editMode ? (
-                    <textarea
-                        className="comment-input"
-                        value={commentText}
-                        onChange={(e) => setCommentText(e.target.value)}
-                        rows={4}
-                    />
+                    <div className="comment-edit-box">
+                        <textarea
+                            className="comment-input"
+                            value={commentText}
+                            onChange={(e) => setCommentText(e.target.value)}
+                            rows={4}
+                        />
+                        <button
+                            className="comment-save-btn"
+                            onClick={handleSave}
+                        >
+                            <Save />
+                            저장
+                        </button>
+                    </div>
                 ) : (
                     <div
                         className="comment-text"
                         onDoubleClick={() => setEditMode(true)}
                         title="더블클릭으로 수정"
                     >
-                        {commentText}
+                        {commentText || "작성된 코멘트가 없습니다."}
                     </div>
                 )}
             </div>
